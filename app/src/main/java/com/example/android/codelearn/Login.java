edb57f9f484428acdd5b9e012121dc5f0f2a71a2
Pavan Kuppa <kppa.pavan@gmail.com>
2018-03-23 22:45:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/codelearn/Login.java
@@ -0,0 +1,17 @@
+package com.example.android.codelearn;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kmurali on 23-03-2018.
+ */
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginsignup);
+    }
+}
